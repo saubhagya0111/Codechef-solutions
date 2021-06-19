87114f6bf29bc035682f08f6fa8db01dd858b666
Saubhagya Chopra <75625298+saubhagya0111@users.noreply.github.com>
2021-06-19 14:40:38 +0530

    Create BUYPLSE.cpp

---
+++ BUYPLSE.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define incr_loop(a, b) for (int i = a; i < b; i++)
+#define decr_loop(a, b) for (int i = b; i > a; i--)
+typedef long long ll;
+#define in(x,y,z,a) cin >> x>>y>>z>>a;
+#define out(x) cout << x;
+#define line cout << endl;
+#define string_in(x) getline(cin, x)
+#define f first
+#define s second
+typedef vector<int> vi;
+typedef vector<char> vc;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+void solution()
+{
+    int i, j, n, m;
+    int a, b, x, y;
+    in(a, b, x, y);
+    out((a * x) + (b * y));
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    solution();
+
+    return 0;
+}

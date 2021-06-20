026d20ae9fc40b3fcfb7357d83a8c3d2f0950300
Saubhagya Chopra <75625298+saubhagya0111@users.noreply.github.com>
2021-06-21 01:00:40 +0530

    Create VISA.cpp

---
+++ VISA.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define incr_loop(a, b) for (int i = a; i < b; i++)
+#define decr_loop(a, b) for (int i = b; i > a; i--)
+#define nested_incr_loop(a, b) for (int j = a; j < b; j++)
+#define nested_decr_loop(a, b) for (int j = b; j > a; --j)
+#define long long int ll
+#define space cout << ' ';
+#define in(x) cin >> x;
+#define in2(x, y) cin >> x >> y;
+#define in3(x, y, z, a, b, c) cin >> x >> y >> z >> a >> b >> c;
+#define out(x) cout << x;
+#define line cout << endl;
+#define str_in(x) getline(cin, x)
+#define f first
+#define s second
+typedef vector<int> vi;
+typedef vector<char> vc;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+void solution()
+{
+    int i, j, n, m;
+    int x1, x2, y1, y2, z1, z2;
+    in3(x1, x2, y1, y2, z1, z2);
+    //Self explanatory :P
+    if (x2 >= x1 && y2 >= y1 && z1 >= z2)
+    {
+        out("YES\n");
+    }
+    else
+    {
+        out("NO\n");
+    }
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    int t = 1;
+    in(t);
+    while (t--)
+    {
+
+        solution();
+    }
+
+    return 0;
+}

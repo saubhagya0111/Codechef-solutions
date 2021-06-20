1eb96f86806d329330b4368a422b08ecd4f997b4
Saubhagya Chopra <75625298+saubhagya0111@users.noreply.github.com>
2021-06-20 21:10:59 +0530

    Create AMR15A.cpp

---
+++ AMR15A.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define incr_loop(a, b) for (int i = a; i < b; i++)
+#define decr_loop(a, b) for (int i = b; i > a; i--)
+#define nested_incr_loop(a, b) for (int j = a; j < b; j++)
+#define nested_decr_loop(a, b) for (int j = b; j > a; --j)
+#define in2(x, y) cin >> x >> y;
+#define in3(x, y, z) cin >> x >> y >> z;
+typedef long long ll;
+#define in(x) cin >> x;
+#define out(x) cout << x;
+#define line cout << endl;
+#define string_in(x) getline(cin, x)
+#define f first
+#define s second
+typedef vector<int> vi;
+typedef vector<char> vc;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+void solution()
+{
+    int i, j, n, m, e = 0, o = 0;
+    in(n);
+    while (n > 0)
+    {
+        int num;
+        in(num);
+        if (num % 2 == 0)
+        {
+            e++;
+        }
+        else if (num % 2 != 0)
+        {
+            o++;
+        }
+        --n;
+    }
+    if (e>o)
+    {
+        out("READY FOR BATTLE\n");
+    }
+    else
+    {
+        out("NOT READY\n");
+    }
+    
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    solution();
+
+    return 0;
+}

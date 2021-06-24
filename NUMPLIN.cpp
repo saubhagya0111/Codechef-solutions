a75b65d38e31cc330d60d391dd50380b80095f1c
Saubhagya Chopra <75625298+saubhagya0111@users.noreply.github.com>
2021-06-25 01:14:36 +0530

    Create NUMPLIN.cpp

---
+++ NUMPLIN.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define incr_loop(a, b) for (int i = a; i < b; i++)
+#define decr_loop(a, b) for (int i = b; i > a; i--)
+#define nested_incr_loop(a, b) for (int j = a; j < b; j++)
+#define nested_decr_loop(a, b) for (int j = b; j > a; --j)
+#define dontquit int main(void)
+#define in2(x, y) cin >> x >> y;
+#define in3(x, y, z) cin >> x >> y >> z;
+typedef long long ll;
+#define in(x) cin >> x;
+#define out(x) cout << x;
+#define line cout << endl;
+#define string_in(x) getline(cin, x)
+#define f first
+#define s second
+typedef vector<int> vi;
+typedef vector<char> vc;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+void solution()
+{
+    int i, j, n, m;
+    in(n);
+    int rev = 0, rem;
+    int temp = n;
+    while (temp>0)
+    {
+        rem = temp % 10;
+        rev = (rev * 10) + rem;
+        temp /= 10;
+    }
+    if (rev==n)
+    {
+        out("YES\n");
+    }
+    else
+    {
+        out("NO\n");
+    }
+    
+}
+dontquit
+{
+
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    solution();
+
+    return 0;
+}

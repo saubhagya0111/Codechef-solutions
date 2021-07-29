12023146c45ce49e88e8f2637aa18440b92f0feb
Saubhagya Chopra <75625298+saubhagya0111@users.noreply.github.com>
2021-07-30 00:20:24 +0530

    Create SUMPOS.cpp

---
+++ SUMPOS.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Problem Code/Link:
+#define incr_loop(i, n) for (i = 0; i < n; i++)
+#define decr_loop(i, n) for (i = n; i > 0; i--)
+#define nested_incr_loop(a, b) for (int j = a; j < b; j++)
+#define nested_decr_loop(a, b) for (int j = b; j > a; --j)
+typedef long long ll;
+#define space cout << ' ';
+#define in(x) cin >> x;
+#define out(x) cout << x;
+#define in3(x, y, z) cin >> x >> y >> z;
+#define in2(x, y) cin >> x >> y;
+#define out3(x, y, z) cout << x << ' ' << y << ' ' << z;
+#define out2(x, y) cout << x << ' ' << y;
+#define line cout << endl;
+#define string_in(x) getline(cin, x)
+#define f first
+#define s second
+typedef vector<int> vi;
+typedef vector<char> vc;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+void solution()
+{
+    int i, j, n, m, n1 = 0;
+    ll a, b, c;
+    in3(a, b, c);
+    if (a + b == c)
+    {
+        out("YES\n");
+    }
+    else if (c + b == a)
+    {
+        out("YES\n");
+    }
+    else if (a + c == b)
+    {
+        out("YES\n");
+    }
+    else
+    {
+        out("NO\n");
+    }
+    
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    int t = 1;
+    in(t);
+
+    while (t--)
+    {
+
+        solution();
+    }
+
+    return 0;
+}

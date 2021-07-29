942006f0a1a67af05b81adc2b2bb57371058fccd
Saubhagya Chopra <75625298+saubhagya0111@users.noreply.github.com>
2021-07-29 12:00:52 +0530

    Create CHFGCD.cpp

---
+++ CHFGCD.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Problem Code/Link:
+#define incr_loop(i, n) for (i = 0; i < n; i++)
+#define decr_loop(i, n) for (i = n; i > 0; i--)
+#define nested_incr_loop(a, b) for (int j = a; j < b; j++)
+#define nested_decr_loop(a, b) for (int j = b; j > a; --j)
+typedef long long ll;
+#define space cout << ' ';
+#define in(x) cin >> x;
+#define out(x) cout << x;
+#define in3(x, y, z) cin >> x >> y >> z;
+#define in2(x, y) cin >> x >> y;
+#define line cout << endl;
+#define string_in(x) getline(cin, x)
+#define f first
+#define s second
+typedef vector<int> vi;
+typedef vector<char> vc;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+void solution()
+{
+    int i, j, n, m, n1 = 0;
+    in2(m, n);
+    ll operation = 0;
+    if (__gcd(m, n) > 1)
+    {
+        operation = 0;
+    }
+    else if (__gcd(m + 1, n) > 1 || __gcd(m, n + 1) > 1)
+    {
+        operation = 1;
+    }
+    else
+    {
+        operation = 2;
+    }
+    out(operation);
+    line;
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    int t = 1;
+    in(t);
+
+    while (t--)
+    {
+
+        solution();
+    }
+
+    return 0;
+}

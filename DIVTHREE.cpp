4f491590e7bba59464cb97790f38ce1e296a82b9
Saubhagya Chopra <75625298+saubhagya0111@users.noreply.github.com>
2021-07-30 10:10:46 +0530

    Create DIVTHREE.cpp

---
+++ DIVTHREE.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Problem Code/Link:DIVTHREE
+#define incr_loop(i, n) for (i = 0; i < n; i++)
+#define decr_loop(i, n) for (i = n; i > 0; i--)
+#define nested_incr_loop(a, b) for (int j = a; j < b; j++)
+#define nested_decr_loop(a, b) for (int j = b; j > a; --j)
+typedef long long ll;
+#define space cout << ' ';
+#define in(x) cin >> x;
+#define out(x) cout << x;
+#define in3(x, y, z) cin >> x >> y >> z;
+#define in2(x, y) cin >> x >> y;
+#define out3(x, y, z) cout << x << ' ' << y << ' ' << z;
+#define out2(x, y) cout << x << ' ' << y;
+#define line cout << endl;
+#define string_in(x) getline(cin, x)
+#define f first
+#define s second
+typedef vector<int> vi;
+typedef vector<char> vc;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+void solution()
+{
+    int i, j, n, m, n1 = 0, k, d;
+    in3(n, k, d);
+    int A[n], sum = 0;
+    incr_loop(n1, n)
+    {
+        in(A[i]);
+        sum += A[i];
+    }
+    int days = sum / k;
+    if (days > d)
+    {
+        days = d;
+    }
+    out(days);
+    line;
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    int t = 1;
+    in(t);
+
+    while (t--)
+    {
+
+        solution();
+    }
+
+    return 0;
+}
